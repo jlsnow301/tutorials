d7004916d67b9b82bc8a30b5671d05159c005250
jlsnow301 <jlsnow.301@gmail.com>
2021-09-22 18:52:19 -0700

    Print formatting

--- GoLang/intro-tutorial/firstapp.go
+++ GoLang/intro-tutorial/firstapp.go
@@ -17,4 +17,27 @@ func main() {
 	var newNumber float64 = float64(luckyNumber) / 3
 
 	fmt.Println(newNumber)
+
+	var defaultFloat float64 = 1.2345678912345678912345678
+	var smallFloat float32 = 1.2345678912345678912345678
+
+	fmt.Println(defaultFloat)
+	fmt.Println(smallFloat)
+
+	var firstRune rune = '$'
+	fmt.Println(firstRune)
+	fmt.Println(string(firstRune))
+
+	var firstByte byte = '$'
+	fmt.Println(firstByte)
+
+	firstName := "Jerm"
+	lastName := "Snow"
+	fullName := fmt.Sprintf("%v %v", firstName, lastName)
+	// Cannot mix types or parse natively?
+	//fmt.Println("9" + 1)
+	age := 33
+
+	fmt.Printf("Hi, I am %v and I am %v (TYPE: %T) years old.", fullName, age, age)
+
 }
