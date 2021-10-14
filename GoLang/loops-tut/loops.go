f5c4c2e145f0cbb6e62cca6fb9a161f22734f1fd
jlsnow301 <jlsnow301@protonmail.com>
2021-10-14 11:58:48 -0700

    Loops practice

--- GoLang/loops-tut/loops.go
+++ GoLang/loops-tut/loops.go
@@ -65,6 +65,7 @@ func getInputNumber() (int, error) {
 	}
 
 	inputNumber = strings.Replace(inputNumber, "\r\n", "", -2)
+
 	chosenNumber, error := strconv.ParseInt(inputNumber, 0, 64)
 
 	if error != nil {		
@@ -74,7 +75,40 @@ func getInputNumber() (int, error) {
 	return int(chosenNumber), nil
 }
 
+func getInputNumberList() {
+	fmt.Println("Please enter a comma separated list of numbers")
+	inputNumberList, error := reader.ReadString('\n')
+	if error != nil {
+		fmt.Println("Invalid input!")
+	}
+	inputNumberList = strings.Replace(inputNumberList, "\n", "", -1)
+	inputNumbers := strings.Split(inputNumberList, ",")
+	sum := 0
+	for index, value := range inputNumbers {
+		fmt.Printf("Index: %v, Value %v\n", index, value)
+		number, _ := strconv.ParseInt(value, 0, 64)
+
+		sum += int(number)
+	}
+	fmt.Printf("Result: %v\n", sum)
+}
+
 func calculateSumUpToNumber()  {
+	fmt.Print("Please enter your number")
+	chosenNumber, error := getInputNumber()
+	if error != nil {
+		fmt.Println("invalid number input!")
+		return
+	}
+	sum := 0
+
+	for index := 1; index <= chosenNumber; index++ {
+		sum = sum + index
+	}
+	fmt.Printf("Result: %v", sum)
+}
+
+func calculateFactorial() {
 	fmt.Print("Please enter your number")
 	chosenNumber, error := getInputNumber()
 
@@ -82,23 +116,26 @@ func calculateSumUpToNumber()  {
 		fmt.Println("invalid number input!")
 		return
 	}
-
-	fmt.Println(chosenNumber)
-	sum := 0
+	factorial := 1
 
 	for index := 1; index <= chosenNumber; index++ {
-		sum = sum + index
+		factorial = factorial * index
 	}
-
-	fmt.Printf("Result: %v", sum)
-}
-
-func calculateFactorial() {
-
+	fmt.Printf("Result: %v", factorial)
 }
 
 func calculateSumManually() {
+	addingNumbers := true
+	sum := 0
+	fmt.Println("Enter your numbers.\nAny non integers will exit.")
+	for addingNumbers {
+		
+		chosenNumber, error := getInputNumber()
+		sum += chosenNumber
 
+		addingNumbers = error == nil
+	}
+	fmt.Printf("Result: %v", sum)
 }
 
 func calculateListSum() {
