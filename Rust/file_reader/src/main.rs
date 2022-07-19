1cd2e646fd43a32bf690e47bc1d111c173443a5b
Jeremiah <jlsnow301@protonmail.com>
2022-07-18 23:16:21 -0700

    io program

---
+++ Rust/file_reader/src/main.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::process;
+
+use file_reader::Config;
+
+fn main() {
+    // --snip--
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = file_reader::run(config) {
+        // --snip--
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
