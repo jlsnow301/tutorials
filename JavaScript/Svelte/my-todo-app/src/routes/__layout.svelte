7c8ee4f239a1e2c876c631a9a5b2543d0ec20f16
Jeremiah <jlsnow301@protonmail.com>
2022-07-20 23:20:24 -0700

    project completed

---
+++ JavaScript/Svelte/my-todo-app/src/routes/__layout.svelte
@@ -0,0 +1,5 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="container mx-auto my-6 max-w-lg"><slot /></div>
---
+++ JavaScript/Svelte/my-todo-app/src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+	// @ts-nocheck
+
+	import TodoForm from '../components/TodoForm.svelte';
+	import Todo from '../components/Todo.svelte';
+	import { todos } from '../stores/todoStore.js';
+</script>
+
+<main>
+	<h1 class="text-2xl fond-bold text-center text-gray-800 md:text-3xl">My Todos</h1>
+	<TodoForm />
+	{#each $todos as todo}
+		<Todo index={todo.id} {todo} />
+	{/each}
+</main>
