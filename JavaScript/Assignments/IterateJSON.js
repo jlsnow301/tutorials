8e86ed3a7dac6799b0b3a6c813e4c479917b66aa
Jeremiah <jlsnow301@protonmail.com>
2022-08-12 12:21:50 -0700

    moving folders

--- JavaScript/Assignments/IterateJSON.js
+++ JavaScript/Assignments/IterateJSON.js
@@ -1,23 +1,22 @@
 // Creates a card for each item (key) in the db.
-  const DisplayKeys = () => {
-    var keys = [];
-    Object.keys(data).forEach((key) =>
-      keys.push(<ComponentName item={data[key]} />)
-    );
-    return keys;
-  };
+const DisplayKeys = () => {
+  var keys = [];
+  Object.keys(data).forEach((key) =>
+    keys.push(<ComponentName item={data[key]} />)
+  );
+  return keys;
+};
 
 // Dynamically displays MongoDB object attributes
-  var hiddenKeys = [];
-  hiddenKeys += "id";
+var hiddenKeys = [];
+hiddenKeys += "id";
 
-  const DisplayDetails = () => {
-    var entries = [];
-    for (let [key, value] of Object.entries(data.item)) {
-      if (hiddenKeys.indexOf(key) >= 0) {
-        continue;
-      }
+const DisplayDetails = () => {
+  var entries = [];
+  Object.entries(data.item).map(([key, value]) => {
+    if (hiddenKeys.indexOf(key) < 0) {
       entries.push(<b>{key}: </b>, `${value}`, <br />);
     }
-    return entries;
-  };
+  });
+  return entries;
+};
