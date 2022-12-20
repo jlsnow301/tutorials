c3f879b51535f2a375bc65a1a52cc92fb34c60b4
Jeremiah <jlsnow301@protonmail.com>
2022-12-19 22:08:56 -0800

    yew

--- Rust/yew-app/src/main.rs
+++ Rust/yew-app/src/main.rs
@@ -1,44 +1,27 @@
+use crate::App::CounterComponent;
 use yew::prelude::*;
 
-enum Msg {
-    AddOne,
-}
+enum Msg {}
 
-struct Model {
-    value: i64,
-}
+struct App {}
 
-impl Component for Model {
+impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { value: 0 }
-    }
-
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
-        match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
-                true
-            }
-        }
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {}
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
         html! {
             <div>
-                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
+               <CounterComponent/>
             </div>
         }
     }
 }
 
 fn main() {
-    yew::start_app::<Model>();
+    yew::start_app::<App>();
 }
