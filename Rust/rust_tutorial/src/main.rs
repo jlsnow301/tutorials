b6fb50684edc129c3e7bd98c78f45d67acc7a77b
Jeremiah <jlsnow301@protonmail.com>
2022-12-26 00:51:49 -0800

    ternary

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -7,14 +7,7 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let age = 22;
-    if (age >= 1) && (age <= 18) {
-        println!("Important Birthday");
-    } else if (age == 21) || (age == 50) {
-        println!("important birthday");
-    } else if age >= 65 {
-        println!("important birthday!");
-    } else {
-        println!("not an important birthday")
-    }
+    let mut my_age = 47;
+    let can_vote = if my_age >= 18 { true } else { false };
+    println!("Can Vote: {}", can_vote);
 }
