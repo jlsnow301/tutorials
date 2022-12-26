0e9fd2b8ef1e51d7c8da6f2da6fa3f12d2d6b71e
Jeremiah <jlsnow301@protonmail.com>
2022-12-25 21:55:43 -0800

    stuff

---
+++ Rust/yew_tutorial/src/main.rs
@@ -0,0 +1,68 @@
+mod details;
+mod tutorial;
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    use details::VideoDetails;
+    use tutorial::{Video, VideosList};
+
+    let videos = vec![
+        Video {
+            id: 1,
+            title: "Building and breaking things".to_string(),
+            speaker: "John Doe".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 2,
+            title: "The development process".to_string(),
+            speaker: "Jane Smith".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 3,
+            title: "The Web 7.0".to_string(),
+            speaker: "Matt Miller".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 4,
+            title: "Mouseless development".to_string(),
+            speaker: "Tom Jerry".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+    ];
+    let selected_video = use_state(|| None);
+
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| selected_video.set(Some(video)))
+    };
+
+    let details = selected_video.as_ref().map(|video| {
+        html! {
+            <VideoDetails video={video.clone()} />
+        }
+    });
+
+    html! {
+        <>
+            <h1>{ "RustConf Explorer" }</h1>
+            <div>
+                <h3>{"Videos to watch"}</h3>
+
+               <VideosList videos={videos} on_click={on_video_select.clone()} />
+            </div>
+            { for details }
+            <div>
+                <h3>{ "John Doe: Building and breaking things" }</h3>
+                <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+            </div>
+        </>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
