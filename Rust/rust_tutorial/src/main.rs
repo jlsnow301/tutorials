6c8346e916ef935ca96f60a7ab52b51de74d4e2a
Jeremiah <jlsnow301@protonmail.com>
2022-12-26 00:43:41 -0800

    floats

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -7,10 +7,11 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    println!("Max u32: {}", u32::MAX);
-    println!("Max u64: {}", u64::MAX);
-    println!("Max usize: {}", usize::MAX);
-    println!("Max u128: {}", u128::MAX);
-    println!("Max f32: {}", f32::MAX);
-    println!("Max f64: {}", f64::MAX);
+    let is_true = true;
+    let my_grade = 'A';
+    let num_1: f32 = 1.111111111111111;
+    let num_2: f64 = 1.111111111111111;
+
+    println!("f32: {}", num_1 + 0.111111111111111);
+    println!("f64: {}", num_2 + 0.111111111111111);
 }
