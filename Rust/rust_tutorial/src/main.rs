19bde5e9ce46b1789b8882e71a654cb3d613f7a2
Jeremiah <jlsnow301@protonmail.com>
2022-12-26 00:57:00 -0800

    ordering

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -7,11 +7,12 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let age2 = 34;
-    match age2 {
-        1..=18 => println!("Important Birthday"),
-        21 | 50 => println!("Important Birthday"),
-        65..=i32::MAX => println!("Important Birthday"),
-        _ => println!("Not an important birthday"),
+    let my_age = 18;
+    let voting_age = 18;
+
+    match my_age.cmp(&voting_age) {
+        Ordering::Less => println!("Can't vote"),
+        Ordering::Greater => println!("Can Vote"),
+        Ordering::Equal => println!("You gained the right to vote"),
     };
 }
