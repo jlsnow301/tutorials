66fb3bae796f338e151cd8aaa401b80f415d08e8
Jeremiah <jlsnow301@protonmail.com>
2022-12-26 00:45:21 -0800

    arithmetic

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -7,11 +7,12 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let is_true = true;
-    let my_grade = 'A';
-    let num_1: f32 = 1.111111111111111;
-    let num_2: f64 = 1.111111111111111;
+    let num_3: u32 = 5;
+    let num_4: u32 = 4;
 
-    println!("f32: {}", num_1 + 0.111111111111111);
-    println!("f64: {}", num_2 + 0.111111111111111);
+    println!("5 + 4 = {}", num_3 + num_4);
+    println!("5 - 4 = {}", num_3 - num_4);
+    println!("5 * 4 = {}", num_3 * num_4);
+    println!("5 / 4 = {}", num_3 / num_4);
+    println!("5 % 4 = {}", num_3 % num_4);
 }
