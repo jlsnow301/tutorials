15075038aedd15c63778839c8565391e69395c95
Jeremiah <jlsnow301@protonmail.com>
2022-12-25 23:15:34 -0800

    max ints

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -7,12 +7,10 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    const ONE_MIL: u32 = 1_000_000;
-    const PI: f32 = 3.141592;
-
-    let age = "34";
-    let mut age: u32 = age.trim().parse().expect("Age wasn't assigned a number");
-
-    age = age + 1;
-    println!("I'm {} and I want ${}", age, ONE_MIL);
+    println!("Max u32: {}", u32::MAX);
+    println!("Max u64: {}", u64::MAX);
+    println!("Max usize: {}", usize::MAX);
+    println!("Max u128: {}", u128::MAX);
+    println!("Max f32: {}", f32::MAX);
+    println!("Max f64: {}", f64::MAX);
 }
