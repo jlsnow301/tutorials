8a795a1ca4e74b8bbfdb0d955b8db565327228c1
Jeremiah <jlsnow301@protonmail.com>
2023-01-03 21:54:33 -0800

    tuples

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -7,28 +7,11 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let arr_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    let mut loop_idx = 0;
-    // println!("1st: {}", arr_1[0]);
-    // println!("Length: {}", arr_1.len());
-    // loop {
-    //     if arr_1[loop_idx] % 2 == 0 {
-    //         loop_idx += 1;
-    //         continue;
-    //     }
-    //     if arr_1[loop_idx] == 9 {
-    //         break;
-    //     }
-    //     println!("Val: {}", arr_1[loop_idx]);
-    //     loop_idx += 1;
-    // }
+    let my_tuple: (u8, String, f64) = (34, "Jerm".to_string(), 50_000.00);
 
-    // while loop_idx < arr_1.len() {
-    //     println!("Arr: {}", arr_1[loop_idx]);
-    //     loop_idx += 1;
-    // }
+    println!("Name: {}", my_tuple.1);
 
-    for val in arr_1.iter() {
-        println!("Val: {}", val);
-    }
+    let (v1, v2, v3) = my_tuple;
+
+    println!("Age: {}", v1);
 }
