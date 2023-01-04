6f30ce7ec729f053b1ec4af769e56d27f78746a5
Jeremiah <jlsnow301@protonmail.com>
2023-01-03 22:17:37 -0800

    more strings!

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -5,18 +5,40 @@ use std::cmp::Ordering;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
+use std::sync::Arc;
 
 fn main() {
-    let mut str1 = String::new();
+    let str3 = String::from("x r t b giogjiro ss");
 
-    str1.push('A');
+    let mut v1: Vec<char> = str3.chars().collect();
 
-    str1.push_str(" word");
+    v1.sort();
+    v1.dedup();
 
-    for word in str1.split_whitespace() {
-        println!("{}", word);
+    for char in v1 {
+        println!("{}", char);
     }
 
-    let str2 = str1.replace("A", "Another");
-    println!("{}", str2);
+    let str4: &str = "Random string";
+
+    let mut str5: String = str4.to_string();
+
+    println!("{}", str5);
+
+    let byte_arr = str5.as_bytes();
+    let st6 = &str5[0..6];
+
+    println!("String length: {}", st6.len());
+
+    str5.clear();
+
+    let str6 = String::from("Just some");
+    let str7 = String::from(" words");
+    let str8 = str6 + &str7;
+
+    println!("{}", str8);
+
+    for char in str8.bytes() {
+        println!("{}", char);
+    }
 }
