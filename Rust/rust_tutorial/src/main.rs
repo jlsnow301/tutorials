3e1ad1cd9739ad457ac656a2a356bb84be81cb68
Jeremiah <jlsnow301@protonmail.com>
2023-01-03 21:52:20 -0800

    loops

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -7,12 +7,28 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 
 fn main() {
-    let my_age = 18;
-    let voting_age = 18;
+    let arr_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut loop_idx = 0;
+    // println!("1st: {}", arr_1[0]);
+    // println!("Length: {}", arr_1.len());
+    // loop {
+    //     if arr_1[loop_idx] % 2 == 0 {
+    //         loop_idx += 1;
+    //         continue;
+    //     }
+    //     if arr_1[loop_idx] == 9 {
+    //         break;
+    //     }
+    //     println!("Val: {}", arr_1[loop_idx]);
+    //     loop_idx += 1;
+    // }
 
-    match my_age.cmp(&voting_age) {
-        Ordering::Less => println!("Can't vote"),
-        Ordering::Greater => println!("Can Vote"),
-        Ordering::Equal => println!("You gained the right to vote"),
-    };
+    // while loop_idx < arr_1.len() {
+    //     println!("Arr: {}", arr_1[loop_idx]);
+    //     loop_idx += 1;
+    // }
+
+    for val in arr_1.iter() {
+        println!("Val: {}", val);
+    }
 }
