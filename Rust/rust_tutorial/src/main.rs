06b2d953dc51bdb3ade4b43163d733c8d9f357b4
Jeremiah <jlsnow301@protonmail.com>
2023-01-04 23:07:44 -0800

    functions

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -7,29 +7,26 @@ use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
 use std::sync::Arc;
 
+fn get_sum(x: i32) -> (i32, i32) {
+    return (x + 1, x + 2);
+}
+
+fn sum_list(list: &[i32]) -> i32 {
+    let mut sum = 0;
+    for &val in list.iter() {
+        sum += &val;
+    }
+
+    return sum;
+}
+
 fn main() {
-    let vec1: Vec<i32> = Vec::new();
-    let mut vec2 = vec![1, 2, 3, 4];
+    // Functions
 
-    vec2.push(5);
+    let (val_1, val_2) = get_sum(3);
 
-    println!("1st: {}", vec2[0]);
+    let num_list = vec![1, 2, 3, 4, 5];
 
-    let second: &i32 = &vec2[1];
-
-    match vec2.get(1) {
-        Some(second) => println!("2nd: {}", second),
-        None => println!("No second val"),
-    }
-
-    for i in &mut vec2 {
-        *i *= 2;
-    }
-    for i in &vec2 {
-        println!("{}", i);
-    }
-
-    println!("Vec length {}", vec2.len());
-
-    println!("Pop : {:?}", vec2.len());
+    println!("Numbers: {}, {}", val_1, val_2);
+    println!("Sum of list = {}", sum_list(&num_list));
 }
