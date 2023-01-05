9c8235ab6a46a4a349272b6acc223bbb709d3596
Jeremiah <jlsnow301@protonmail.com>
2023-01-04 22:34:40 -0800

    enums

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -8,10 +8,32 @@ use std::io::{BufRead, BufReader, ErrorKind, Write};
 use std::sync::Arc;
 
 fn main() {
-    // casting
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
 
-    let int_u8: u8 = 5;
-    let int2_u8: u8 = 5;
-    let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
-    println!("{}", int3_u32);
+    impl Day {
+        fn is_weekend(&self) -> bool {
+            match self {
+                Day::Saturday | Day::Sunday => true,
+                _ => false,
+            }
+        }
+    }
+
+    let today: Day = Day::Monday;
+    match today {
+        Day::Monday => println!("Everone hates monday!"),
+        Day::Thursday => println!("Almost there!"),
+        Day::Friday => println!("Weekend!"),
+        _ => println!("Another day"),
+    }
+
+    println!("Is today a weekend? {}", today.is_weekend());
 }
