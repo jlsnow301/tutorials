0067ad554670ce059934dc1338e131c9503f6a2b
Jeremiah <jlsnow301@protonmail.com>
2023-01-04 23:23:51 -0800

    borrow checker

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -5,18 +5,32 @@ use std::cmp::Ordering;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, BufReader, ErrorKind, Write};
+use std::ops::Add;
 use std::process::Output;
 use std::sync::Arc;
 
-use std::ops::Add;
+fn print_str(x: String) {
+    println!("A string: {}", x);
+}
 
-fn get_sum_gen<Tnum: Add<Output = Tnum>>(x: Tnum, y: Tnum) -> Tnum {
-    return x + y;
+fn print_return_str(x: String) -> String {
+    println!("A string {}", x);
+    x
+}
+
+fn change_string(name: &mut String) {
+    name.push_str(" is happy");
+    println!("Message: {}", name);
 }
 
 fn main() {
-    // generic basics
+    let str1 = String::from("World");
+    let str2 = str1.clone();
+    // print_str(str1);
+    let str3 = print_return_str(str1);
+    println!("str3 = {}", str3);
 
-    println!("5 + 4 = {}", get_sum_gen(5, 4));
-    println!("5.2 + 4.6 = {}", get_sum_gen(5.2, 4.6));
+    let mut str4 = String::from("Jerm");
+
+    change_string(&mut str4);
 }
