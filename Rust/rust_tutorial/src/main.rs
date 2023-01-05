125fe387bf6c4b08c5864bf89f827cd7ec78e84a
Jeremiah <jlsnow301@protonmail.com>
2023-01-04 22:29:27 -0800

    casting

--- Rust/rust_tutorial/src/main.rs
+++ Rust/rust_tutorial/src/main.rs
@@ -8,37 +8,10 @@ use std::io::{BufRead, BufReader, ErrorKind, Write};
 use std::sync::Arc;
 
 fn main() {
-    let str3 = String::from("x r t b giogjiro ss");
+    // casting
 
-    let mut v1: Vec<char> = str3.chars().collect();
-
-    v1.sort();
-    v1.dedup();
-
-    for char in v1 {
-        println!("{}", char);
-    }
-
-    let str4: &str = "Random string";
-
-    let mut str5: String = str4.to_string();
-
-    println!("{}", str5);
-
-    let byte_arr = str5.as_bytes();
-    let st6 = &str5[0..6];
-
-    println!("String length: {}", st6.len());
-
-    str5.clear();
-
-    let str6 = String::from("Just some");
-    let str7 = String::from(" words");
-    let str8 = str6 + &str7;
-
-    println!("{}", str8);
-
-    for char in str8.bytes() {
-        println!("{}", char);
-    }
+    let int_u8: u8 = 5;
+    let int2_u8: u8 = 5;
+    let int3_u32: u32 = (int_u8 as u32) + (int2_u8 as u32);
+    println!("{}", int3_u32);
 }
