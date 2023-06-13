bc914f6eb41c9b5fc1ca0110a60f32a7727043f0
Jeremiah <jlsnow301@pm.me>
2023-06-12 21:08:57 -0700

    front end masters course

---
+++ Rust/fem_course/src/main.rs
@@ -0,0 +1,73 @@
+use std::{fmt::Display, str::FromStr};
+
+use shapes::collisions::{Collidable, Contains, Points};
+
+use crate::shapes::{circle::Circle, rect::Rect};
+
+mod shapes;
+
+enum Shape {
+    Circle(Circle),
+    Rect(Rect),
+}
+
+impl FromStr for Shape {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (shape, data) = s.split_once(" ").unwrap_or(("", ""));
+
+        match shape {
+            "rect" => return Ok(Shape::Rect(data.parse()?)),
+            "circle" => return Ok(Shape::Circle(data.parse()?)),
+            _ => return Err(anyhow::anyhow!("bad shape")),
+        }
+    }
+}
+
+impl Points for &Shape {
+    fn points(&self) -> shapes::collisions::PointIter {
+        match self {
+            Shape::Circle(c) => return c.points(),
+            Shape::Rect(r) => return r.points(),
+        }
+    }
+}
+
+impl Contains for &Shape {
+    fn contains_point(&self, point: (f64, f64)) -> bool {
+        match self {
+            Shape::Circle(c) => return c.contains_point(point),
+            Shape::Rect(r) => return r.contains_point(point),
+        }
+    }
+}
+
+impl Display for Shape {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Shape::Circle(c) => return write!(f, "{}", c),
+            Shape::Rect(r) => return write!(f, "{}", r),
+        }
+    }
+}
+
+fn main() {
+    let shapes = std::fs::read_to_string("shapes.txt")?
+        .lines()
+        .filter_map(|x| x.parse::<Shape>().ok())
+        .collect::<Vec<_>>();
+
+    shapes
+        .iter()
+        .skip(1)
+        .zip(shapes.iter().take(shapes.len() - 1))
+        .filter(|(a, b)| a.collide(b))
+        .for_each(|(a, b)| {
+            println!("{} collides with {}", a, b);
+        });
+
+    println!("{:?}", shapes);
+
+    return;
+}
